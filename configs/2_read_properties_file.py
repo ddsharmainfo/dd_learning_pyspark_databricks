ddd531330ff992d384c677fba58d09da6dde42ae
DD Sharma <ddsharmainfo@gmail.com>
2023-12-05 21:23:28 +0530

    updated configs

--- configs/2_read_properties_file.py
+++ configs/2_read_properties_file.py
@@ -1,7 +1,11 @@
+import os
 import configparser
 
+DIR = os.path.dirname(__file__)
+CONFIG_FILE = os.path.join(DIR, './2_config.properties')
+
 config = configparser.ConfigParser()
-config.read('./1_config.properties')
+config.read(CONFIG_FILE)
 
 print('\n ===== Dev configs =====')
 ENV_VAR = 'DEV'
@@ -12,14 +16,14 @@ print('Value of param2 is = ', param2)
 
 print('\n ===== QA configs =====')
 ENV_VAR = 'QA'
-param1 = config.get(ENV_VAR, "param1")
-param2 = config.get(ENV_VAR, "param2")
-print('Value of param1 is = ', param1)
-print('Value of param2 is = ', param2)
+param3 = config.get(ENV_VAR, "param3")
+param4 = config.get(ENV_VAR, "param4")
+print('Value of param3 is = ', param3)
+print('Value of param4 is = ', param4)
 
 print('\n===== PROD configs =====')
 ENV_VAR = 'PROD'
-param1 = config.get(ENV_VAR, "param1")
-param2 = config.get(ENV_VAR, "param2")
-print('Value of param1 is = ', param1)
-print('Value of param2 is = ', param2)
+param5 = config.get(ENV_VAR, "param5")
+param6 = config.get(ENV_VAR, "param6")
+print('Value of param5 is = ', param5)
+print('Value of param6 is = ', param6)
--- configs/get_configs.py
+++ configs/get_configs.py
@@ -1,7 +1,12 @@
+import os
 import configparser
 
+
+DIR = os.path.dirname(__file__)
+CONFIG_FILE = os.path.join(DIR, './configs.conf')
+
 config = configparser.ConfigParser()
-config.read('./configs.conf')
+config.read(CONFIG_FILE)
 
 db_connect = config.get('IDE', "db_connect")
 print('Param db_connect value:', db_connect)
