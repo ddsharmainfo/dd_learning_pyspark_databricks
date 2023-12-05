65c814745a1dc31a7a4493bd0bdc9b724ecadd12
DD Sharma <ddsharmainfo@gmail.com>
2023-12-05 21:29:29 +0530

    updated configs

--- configs/4_read_config_python_file.py
+++
@@ -1,9 +0,0 @@
-import config_python as conf
-
-print('\n ===== configs =====')
-
-print(conf.DEV["key"])
-
-print(conf.QA["key"])
-
-print(conf.PROD["key"])
--- configs/config_python.py
+++
@@ -1,5 +0,0 @@
-DEV = {"key": "value from DEV"}
-
-QA = {"key": "value from QA"}
-
-PROD = {"key": "value from PROD"}
