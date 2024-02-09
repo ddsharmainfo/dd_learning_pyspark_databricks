094e172f2341be8b643f717dee8885f78841c915
DD Sharma <ddsharmainfo@gmail.com>
2024-02-09 22:12:10 +0530

    formatted modules example

--- src_databricks_python_modules/wrapper/run_modules_using_singleton.py
+++ src_databricks_python_modules/wrapper/run_modules_using_singleton.py
@@ -1,6 +1,5 @@
-from src_databricks_python_modules.get_spark_session_singleton import SparkSessionSingleton get_spark_session
-# from src_databricks_python_modules.module1 import run_range_df
-# from src_databricks_python_modules.module2.run_range_df import get_range_df
+from src_databricks_python_modules.get_spark_session_singleton import SparkSessionSingleton
+from src_databricks_python_modules.module1 import run_range_df
 
 
 def get_range_df(spark_session):
@@ -10,8 +9,14 @@ def get_range_df(spark_session):
 
 
 if __name__ == "__main__":
-    print('run_range_df started')
+    print('main run_modules_using_singleton started')
+
     spark = SparkSessionSingleton.get_spark_session()
-    df = get_range_df(spark)
-    df.show()
-    print('run_range_df ended')
+
+    df1 = get_range_df(spark)
+    df1.show()
+
+    df2 = run_range_df.get_range_df(spark)
+    df2.show()
+    
+    print('main run_modules_using_singleton ended')
